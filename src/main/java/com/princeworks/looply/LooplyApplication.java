ad3fa23e153c2be3c42efd03ef2f2aad0957a39a
Prince Pal <rishabpal2000@gmail.com>
2025-10-25 19:39:33 +0530

    added required packages

---
+++ src/main/java/com/princeworks/looply/LooplyApplication.java
@@ -0,0 +1,13 @@
+package com.princeworks.looply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LooplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LooplyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/princeworks/looply/LooplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.princeworks.looply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LooplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
